f2e1881835b38ee112f4cbbaf909cdd138d0eac8
Ángel Gonzalez <angeladolfogonzalezarana@gmail.com>
2023-11-09 13:02:34 -0400

    agregando index.js y app.js al acceso principal

---
+++ App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Principal from './componentes/Principal';
+import Contenido from './componentes/Contenido';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Principal></Principal>}></Route>
+        <Route path='/:id' element={ <Contenido></Contenido> }></Route>
+      </Routes>
+    </BrowserRouter>
+      
+    </>
+  );
+}
+
+export default App;
